ce603afd29ea84a85ae3c850a1db4a9016ebebe1
Lukas Carvajal <lukascarvajal@gmail.com>
2015-06-10 05:16:45 -0400

    Initial Commit

---
+++ News-Hack/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  News-Hack
+//
+//  Created by Lukas Carvajal on 6/10/15.
+//  Copyright (c) 2015 Lukas Carvajal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ News-Hack/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  News-Hack
+//
+//  Created by Lukas Carvajal on 6/10/15.
+//  Copyright (c) 2015 Lukas Carvajal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
